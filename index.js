3ceb2728001e02c880485ad167c48f64fa430992
SAJADH S <sajadh2001s@gmail.com>
2023-03-04 10:45:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var tom =new Audio("sounds/tom-1.mp3");
+var tom2=new Audio("sounds/tom-2.mp3");
+var tom3=new Audio("sounds/tom-3.mp3");
+var tom4=new Audio("sounds/tom-4.mp3");
+var bass=new Audio("sounds/kick-bass.mp3");
+var crash=new Audio("sounds/crash.mp3");
+var snare=new Audio("sounds/snare.mp3");
 var drums=document.querySelectorAll(".drum").length;
 var i=0;
 while(i<drums)
@@ -18,38 +25,29 @@ document.addEventListener("keypress",function(event)
 
 
 })
-
 function sound(key){
 
 switch (key) {
     case "w":
-        var audio=new Audio("sounds/tom-1.mp3");
-        audio.play();
-        
+        tom.play();
         break;
     case "a":
-        var audio=new Audio("sounds/tom-2.mp3");
-        audio.play();
+        tom2.play();
         break;
      case "s":
-        var audio=new Audio("sounds/tom-3.mp3");
-        audio.play();
+        tom3.play();
         break;
     case "d":
-        var audio=new Audio("sounds/tom-4.mp3");
-        audio.play();
+        tom4.play();
         break;
     case "j":
-        var audio=new Audio("sounds/kick-bass.mp3");
-        audio.play();
+        bass.play();
         break;
      case "k":
-        var audio=new Audio("sounds/crash.mp3");
-        audio.play();
+        crash.play();
         break;
     case "l":
-        var audio=new Audio("sounds/snare.mp3");
-        audio.play();
+        snare.play();
         break;
     default:
         console.log("press valid inputs (w,a,s,d,j,k,l) only!!!!");
